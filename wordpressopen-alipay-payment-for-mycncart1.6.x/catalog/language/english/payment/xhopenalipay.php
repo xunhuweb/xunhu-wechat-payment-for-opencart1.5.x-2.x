9598e28a6cd611e4086a580cb16b95fc90484222
hoter <hoter@xunhuweb.com>
2018-04-16 18:23:03 +0800

    rmove unused files
    rmove unused files

--- wordpressopen-alipay-payment-for-mycncart1.6.x/catalog/language/english/payment/xhopenalipay.php
+++
@@ -1,5 +0,0 @@
-<?php
-$_['text_title'] = '<div><img src="/catalog/view/theme/default/xhopenalipay/images/logo.png"/></div>';
-$_['button_confirm'] ='Confirm Order';
-$_['button_loading']='Loading...';
-$_['button_back'] ='Cancel';
